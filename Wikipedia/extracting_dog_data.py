d6d19d3b396cd2da677a0c2695778c7c4eff3bf2
Halle Burns <34111216+hburns2@users.noreply.github.com>
2018-12-16 17:02:17 -0600

    adding code comments

--- Wikipedia/extracting_dog_data.py
+++ Wikipedia/extracting_dog_data.py
@@ -1,23 +1,18 @@
-# use GLOB
-# export csv
-# T/F
-# title/page id
-
 import json
 import csv
 import os
 import glob
 
+# Using glob.glob to find any filepath in the directory that ends in .json
 allfiles = glob.glob('*.json')
 
+# Printing a list of all files ending in .json in that directory
 print(allfiles)
-# import os
-# going to create a list i.e. listdir(source)
-# for file in filepath + file
+
 allrows = []
 for path in allfiles:
 
-    infile = open(path, "r")
+    infile = open(path, "r")    # Creating a for-loop that will loop over all files in the directory and read in their text
     text = infile.read()
     infile.close()
 
@@ -36,6 +31,7 @@ for path in allfiles:
         row.append(snippet)
         allrows.append(row)
 
+# Creating an outfile containing the combined text from all .json files in the directory
 headers = ['path','pagetitle', 'pageid', 'wordcount', 'snippet']
 
 with open('allresults.csv', 'w', newline='') as outfile:
