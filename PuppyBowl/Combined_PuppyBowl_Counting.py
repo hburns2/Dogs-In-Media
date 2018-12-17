4421dc91879a1122bdc8c8122370b432a2f8913c
Halle Burns <34111216+hburns2@users.noreply.github.com>
2018-12-17 17:43:47 -0600

    adding comments

--- PuppyBowl/Combined_PuppyBowl_Counting.py
+++ PuppyBowl/Combined_PuppyBowl_Counting.py
@@ -1,11 +1,12 @@
 import csv
 from collections import Counter
 
+# reading in my combined data file with breeds from all of the years I am investigating
 with open("Combined_PuppyBowl_Datafiles.csv", "r") as infile:
     csv_reader = csv.reader(infile)
-    words_list = list(csv_reader)
+    words_list = list(csv_reader)       # Making my data into a list
 
-word_counts = Counter()
+word_counts = Counter()         # Using Counter and a for-loop to figure out how many times each breed appeared
 for line in words_list:
     for word in line:
         word_counts[word] = word_counts.get(word, 0) + 1
